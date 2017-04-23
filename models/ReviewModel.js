da9b224f6e9229eece3d3ccf6e62c1150f7d9c0e
ruihan-jia <rick.ruihan.jia@gmail.com>
2017-04-23 22:35:09 +0000

    review api changes

---
+++ models/ReviewModel.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var ReviewSchema = new mongoose.Schema({
+  cid: String,
+  year: Number,
+  hard: Number,
+  useful: Number,
+  interest: Number,
+  comment: String,
+  user_ip: String,
+  updated_at: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model('Review', ReviewSchema);
+
--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var Course = require('../models/CourseModel.js');
+var Review = require('../models/ReviewModel.js');
 
 /* GET /todos listing. */
-router.get('/', function(req, res, next) {
+router.get('/course', function(req, res, next) {
   if(!req.query.cid){
     Course.find(function (err, courses) {
       if (err) return next(err);
@@ -13,20 +14,103 @@ router.get('/', function(req, res, next) {
     });
   }else {
     console.log("api received " + req.query.cid);
-    Course.findOne({cid:req.query.cid}, function (err, post) {
+    var querycid = req.query.cid.toUpperCase();
+    console.log("query cid: " + querycid);
+    Course.findOne({cid:querycid}, function (err, post) {
       if (err) return next(err);
       res.json(post);
-  });
+    });
 
   }
 });
 
 /* POST /todos */
-router.post('/', function(req, res, next) {
+router.post('/course', function(req, res, next) {
   Course.create(req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
   });
+});
+
+
+
+/* GET /todos listing. */
+router.get('/review', function(req, res, next) {
+  if(!req.query.cid){
+    Review.find(function (err, courses) {
+      if (err) return next(err);
+    
+      res.json(courses);
+    });
+  }else {
+    var querycid = req.query.cid.toUpperCase();
+    console.log("retrieve reviews of " + querycid);
+
+    var response = {};
+    var course = 'CourseInfo';
+    var review = 'CourseReviews';
+    response[course] = [];
+    response[review] = [];
+
+    var courseInfo;
+
+    Course.findOne({cid:querycid}, function (err, post) {
+      if (err) return next(err);
+      console.log(post);
+      //console.log(JSON.stringify(post));
+      console.log(post.toJSON());
+      //console.log(post.toObject());
+      response['CourseInfo'].push(post);
+    });
+
+
+    Review.find({cid:querycid}, function (err, post) {
+      if (err) return next(err);
+      //response[review].push(JSON.stringify(post));
+    });
+
+    console.log(response);
+
+    res.json(response);
+  }
+});
+
+
+
+/* POST /reviews */
+router.post('/review', function(req, res, next) {
+  var cid = req.body.cid.toUpperCase();
+  var year = req.body.year;
+  var hard = req.body.hard;
+  var useful = req.body.useful;
+  var interest = req.body.interest;
+  var comment = req.body.comment;
+  var ip = req.ip;
+  var query = "cid: '" + cid + "', year: '" + year + "', hard: '" + hard + "', useful: '" + useful + "', interest: '" + interest + "', comment: '" + comment + "', user_ip: '" + ip + "'";
+
+
+  if(!cid || !year || !hard || !useful || !interest || !comment || !ip){
+    console.log("invalid request");
+    res.json("invalid request");
+  } else {
+    console.log(query);
+    Review.create({cid: cid, year: year, hard: hard, useful: useful, interest: interest, comment: comment, user_ip: ip}, function (err, post) {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+      console.log("test");
+      res.json(post);
+    });
+
+  }
+
+/*
+  Review.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+*/
 });
 
 /* GET /todos/id */
