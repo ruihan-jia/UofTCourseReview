dea4cdde34c6b59ac0b8c09865d556ad33613543
ruihan-jia <rick.ruihan.jia@gmail.com>
2017-04-28 07:10:53 +0000

    star rating system wip

--- public/javascripts/utcrApp.controllers.js
+++ public/javascripts/utcrApp.controllers.js
@@ -1,68 +1,95 @@
 
 
-        angular.module('utcrApp').controller('HomeController', ['$scope', 'Courses', '$location', function ($scope, Courses, $location) {
-          //$scope.editing = [];
-          //$scope.courses = Courses.query();
-          $scope.search = function(){
-            $location.url('/course/' + $scope.courseID);
-          }
-        }])
-        .controller('CourseController', ['$scope', '$routeParams', 'Courses', 'Reviews', '$location', function ($scope, $routeParams, Courses, Reviews, $location) {
+angular.module('utcrApp')
 
-          $scope.courseID = $routeParams.id;
-          Reviews.get({cid: $routeParams.id }, function(data) {
-            console.log(data);
-	    console.log(data.CourseInfo);
-	    var jsonres = data;
-            $scope.courseResponse = jsonres.CourseInfo;
-            $scope.reviewResponse = jsonres.CourseReviews;
+    .controller('HomeController', ['$scope', 'Courses', '$location', function ($scope, Courses, $location) {
+        //$scope.editing = [];
+        //$scope.courses = Courses.query();
+        $scope.search = function(){
+          $location.url('/course/' + $scope.courseID);
+        }
+    }])
+    
+
+    .controller('CourseController', ['$scope', '$routeParams', 'Courses', 'Reviews', '$location', function ($scope, $routeParams, Courses, Reviews, $location) {
+
+        $scope.courseID = $routeParams.id;
+
+        Reviews.get({cid: $routeParams.id }, function(data) {
+          console.log(data);
+	  console.log(data.CourseInfo);
+	  var jsonres = data;
+          $scope.courseResponse = jsonres.CourseInfo;
+          $scope.reviewResponse = jsonres.CourseReviews;
  
-          });
-        }])
-        .controller('RatingController', function($scope) {
-          $scope.rating = 5;
-          $scope.rateFunction = function(rating) {
-            alert('Rating selected - ' + rating);
-          };
-        })
+        });
+
+        $scope.rating = 0;
+    	$scope.ratings = [
+	  {
+	    namef: 'easy',
+	    namel: 'hard',
+            current: 1,
+            max: 5
+    	  }, 
+	  {
+	    namef: 'useless',
+	    namel: 'useful',
+            current: 1,
+            max: 5
+    	  },
+	  {
+	    namef: 'boring',
+	    namel: 'interesting',
+	    current: 1,
+	    max: 5
+	  }
+	];
+
+    	$scope.getSelectedRating = function (rating) {
+          console.log(rating);
+    	}
+
+    }])
+
+//star rating taken from: http://stackoverflow.com/questions/23646395/rendering-a-star-rating-system-using-angularjs
+
+    .directive('starRating', function () {
+    return {
+        restrict: 'A',
+        template: '<ul class="rating">' +
+            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+            '\u2605' +
+            '</li>' +
+            '</ul>',
+        scope: {
+            ratingValue: '=',
+            max: '=',
+            onRatingSelected: '&'
+        },
+        link: function (scope, elem, attrs) {
+            var updateStars = function () {
+                scope.stars = [];
+                for (var i = 0; i < scope.max; i++) {
+                    scope.stars.push({
+                        filled: i < scope.ratingValue
+                    });
+                }
+            };
+            scope.toggle = function (index) {
+                scope.ratingValue = index + 1;
+                scope.onRatingSelected({
+                    rating: index + 1
+                });
+            };
+            scope.$watch('ratingValue', function (oldVal, newVal) {
+                if (newVal) {
+                    updateStars();
+                }
+            });
+        }
+      }
+    });
 
 
-/*
-        .directive('starRating',
-	function() {
-	  return {
-	    restrict : 'A',
-	    template : '<ul class="rating">' + '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' + '\u2605' + '</li>' + '</ul>',
-	    scope : {
-	      ratingValue : '=',
-	      max : '=',
-	      onRatingSelected : '&'
-	    },
-            link : function(scope, elem, attrs) {
-	      var updateStars = function() {
-	        scope.stars = [];
-		  for ( var i = 0; i < scope.max; i++) {
-		    scope.stars.push({
-	  	    filled : i < scope.ratingValue
-		  });
-	        }
-	      };
-				
-	      scope.toggle = function(index) {
-	        scope.ratingValue = index + 1;
-	        scope.onRatingSelected({
-	          rating : index + 1
-	        });
-	      };
-				
-	      scope.$watch('ratingValue',
-	      function(oldVal, newVal) {
-	        if (newVal) {
-		  updateStars();
-		}
-	      });
-	    }
-	  };
-	}
-*/
 
